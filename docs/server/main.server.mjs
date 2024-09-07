ab7397ed0b990a1cd1b33022f6dc2d344cefdd57
AlexandreSalim <alexandresalim7@gmail.com>
2024-09-07 13:08:53 -0300

    quase terminando

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-DCXTAPCY.mjs";import"./chunk-RCRD4SBX.mjs";import"./chunk-XZ657ZIA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-AGXO6QLW.mjs";import"./chunk-RCRD4SBX.mjs";import"./chunk-XZ657ZIA.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
